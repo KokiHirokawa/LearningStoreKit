c737097133d00f652b3923833213a253a2b38385
pihero <kitakitsune1110@gmail.com>
2022-07-30 12:30:26 +0900

    Add button to purchase coins

--- LearningStoreKit/ContentView.swift
+++ LearningStoreKit/ContentView.swift
@@ -1,16 +1,30 @@
-//
-//  ContentView.swift
-//  LearningStoreKit
-//
-//  Created by 廣川 昂紀 on 2022/07/30.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 24) {
+            Button {
+                // - TODO: Purchase 100 coins
+            } label: {
+                Text("100 coins (StoreKit 1)")
+                    .font(.system(size: 14, weight: .bold))
+                    .frame(width: 200, height: 64)
+            }
+            .background(Color.black)
+            .cornerRadius(8)
+            .foregroundColor(Color.white)
+
+            Button {
+                // - TODO: Purchase 100 coins
+            } label: {
+                Text("100 coins (StoreKit 2)")
+                    .font(.system(size: 14, weight: .bold))
+                    .frame(width: 200, height: 64)
+            }
+            .background(Color.black)
+            .cornerRadius(8)
+            .foregroundColor(Color.white)
+        }
     }
 }
 
