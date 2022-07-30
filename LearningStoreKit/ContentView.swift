bdc9e28466821e20bf551a1018a3f86fd3d38664
pihero <kitakitsune1110@gmail.com>
2022-07-30 12:19:02 +0900

    Create project

---
+++ LearningStoreKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningStoreKit
+//
+//  Created by 廣川 昂紀 on 2022/07/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningStoreKit/LearningStoreKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningStoreKitApp.swift
+//  LearningStoreKit
+//
+//  Created by 廣川 昂紀 on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct LearningStoreKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
